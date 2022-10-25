c05ea4e09c6b2490b5faf78457d5f995bfeecb35
Paul Wieland <p@ul.sickdimension.com>
2022-10-25 11:24:19 -0400

    Initial logic

--- src/HotTubControl.cpp
+++ src/HotTubControl.cpp
@@ -1,9 +1,208 @@
 #include "HotTubControl.h"
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(76800);
+  bootstrapManager.bootstrapSetup(manageDisconnections, manageHardwareButton, callback);
+  bootstrapManager.setMQTTWill(tubStatusTopic.c_str(),"\"offline\"",1,false,true);
+  
+  // DS18B20 one wire temperature sensor
+  sensors.begin();
+
+  Serial.print("tubCommandTopic: ");
+  Serial.println(tubCommandTopic);
+  Serial.print("tubStatusTopic: ");
+  Serial.println(tubStatusTopic);
+
+
+  pinMode(HEAT1,OUTPUT);
+  pinMode(HEAT2,OUTPUT);
+  pinMode(PUMPLOW,OUTPUT);
+  pinMode(PUMPHIGH,OUTPUT);
+  pinMode(TEMPERATURE,INPUT);
+
+  pinMode(PRESSURE,INPUT_PULLUP);
+  pinMode(STATUS_PUMPLOW,INPUT_PULLUP);
+  pinMode(STATUS_PUMPHIGH,INPUT_PULLUP);
+  pinMode(STATUS_HEAT1,INPUT_PULLUP);
+
+  attachInterrupt(PRESSURE,isrPressureDetected,CHANGE);
+  attachInterrupt(STATUS_HEAT1,isrHeat1,CHANGE);
+  attachInterrupt(STATUS_PUMPLOW,isrPumpLow,CHANGE);
+  attachInterrupt(STATUS_PUMPHIGH,isrPumpHigh,CHANGE);
+
+
+  digitalWrite(HEAT1,HIGH);
+  digitalWrite(HEAT2,LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (isConfigFileOk){
+    // Bootsrap loop() with Wifi, MQTT and OTA functions
+    bootstrapManager.bootstrapLoop(manageDisconnections, manageQueueSubscription, manageHardwareButton);
+
+    if(!setupComplete){
+      setupComplete = true;
+      // Broadcast that we are online
+      bootstrapManager.publish(tubStatusTopic.c_str(), "\"online\"", false);
+    }
+  }
+
+  // read the temp sensor + actual relay status from optocoupler and broadcast on mqtt
+  statusUpdateLoop();
+  
+  // If the temperature too low, or too much time has passed since last cycle start the low speed pump
+
+
+  // if pump is running, actual temp lower than target temp, turn heat on
+  if(statusPressure && actualTemperature < targetTemperature){
+    // avoid rapid cycling the heater. wait at least 60 seconds since last turn on event before turning on again
+    if(millis() > (lastHeatMillis + 6000)){ 
+      digitalWrite(HEAT1,HIGH);
+      digitalWrite(HEAT2,HIGH);
+      lastHeatMillis = millis();
+      Serial.println("Heat On");
+    //}else if(millis() - lastHeatMillis > 59500){
+    }else if(digitalRead(HEAT1) == LOW){
+      Serial.print("Heat request ignored, waiting");
+      Serial.print(millis() - lastHeatMillis);
+      Serial.println(" millis...");
+    }
+  }else if(digitalRead(HEAT1) == HIGH){
+    digitalWrite(HEAT1,LOW);
+    digitalWrite(HEAT2,LOW);
+    Serial.println("Heat Off");
+  }
+}
+
+void statusUpdateLoop(){
+  static long unsigned int lastTempMillis = 0;
+  static bool lastStatusPressure = false;
+  static bool lastStatusPumpLow = false;
+  static bool lastStatusPumpHigh = false;
+  static bool lastStatusHeat1 = false;
+
+  if(millis() > (lastTempMillis + statusTempInterval) ){
+    updateTemperatureLoop();
+
+    lastTempMillis = millis();
+    Serial.print("Current temperature: ");
+    Serial.println(actualTemperature);
+    Serial.print(" Target temperature: ");
+    Serial.println(targetTemperature);
+    bootstrapManager.publish(String(tubStatusTopic + "/temperature").c_str(), String(actualTemperature).c_str(), false);
+  }
+
+  if(lastStatusPressure != statusPressure){
+    Serial.print("Pressure Status Change to: ");
+    Serial.println(statusPressure);
+    bootstrapManager.publish(String(tubStatusTopic + "/pressure").c_str(), String(statusPressure).c_str(), false);
+  }
+
+  if(lastStatusPumpLow != statusPumpLow){
+    Serial.print("Pump Low Status Change to: ");
+    Serial.println(statusPumpLow);
+    bootstrapManager.publish(String(tubStatusTopic + "/pump_low").c_str(), String(statusPumpLow).c_str(), false);
+  }
+
+  if(lastStatusPumpHigh != statusPumpHigh){
+    Serial.print("Pump High Status Change to: ");
+    Serial.println(statusPumpHigh);
+    bootstrapManager.publish(String(tubStatusTopic + "/pump_high").c_str(), String(statusPumpHigh).c_str(), false);
+  }
+
+  if(lastStatusHeat1 != statusHeat1){
+    Serial.print("Heat1 Status Change to: ");
+    Serial.println(statusHeat1);
+    bootstrapManager.publish(String(tubStatusTopic + "/heat1").c_str(), String(statusHeat1).c_str(), false);
+  }
+
+  lastStatusPressure = statusPressure;
+  lastStatusPumpLow = statusPumpLow;
+  lastStatusPumpHigh = statusPumpHigh;
+  lastStatusHeat1 = statusHeat1;
+}
+
+void IRAM_ATTR isrPressureDetected(){
+    // static unsigned long lastInterruptTime = 0;
+    // unsigned long currentMillis = millis();
+
+    // Prevent ISR during the first 2 seconds after reboot
+    // if(currentMillis < 2000) return;
+    
+    // if(currentMillis - lastInterruptTime > 5000){
+      statusPressure = !digitalRead(PRESSURE);
+    // }
+
+  // lastInterruptTime = currentMillis;
+}
+
+// When the input pin changes, read the 
+void IRAM_ATTR isrPumpLow(){
+  statusPumpLow = !digitalRead(STATUS_PUMPLOW);
+}
+
+void IRAM_ATTR isrPumpHigh(){
+  statusPumpHigh = !digitalRead(STATUS_PUMPHIGH);
+}
+
+void IRAM_ATTR isrHeat1(){
+  statusHeat1 = !digitalRead(STATUS_HEAT1);
+}
+
+
+void updateTemperatureLoop(){
+  // static unsigned int lastMillis = 0;
+  // unsigned int currentMillis = millis();
+
+  // if(currentMillis - lastMillis > 5000){
+    sensors.requestTemperatures();
+    float tempReading = sensors.getTempFByIndex(0);
+    if(tempReading > -100){
+      actualTemperature = tempReading;
+    }else{
+      Serial.println("Temperature sensor error!");
+    }
+    // lastMillis = currentMillis;
+
+    // Serial.print("temp reading: ");
+    // Serial.println(actualTemperature);
+  // }
+}
+
+void callback(char *topic, byte *payload, unsigned int length){
+  Serial.println("mqtt callback");
+
+  String topicPrefix = tubCommandTopic;
+  topicPrefix.remove(topicPrefix.length() - 1,1);
+
+  // for (int i=0;i<length;i++) {
+  //   // Serial.print((char)payload[i]);
+  //   Serial.println(payload[i], HEX);
+  // }
+
+
+  if (String(topic) == String(topicPrefix + "temperature")){
+    char rxVal[length];
+    Serial.println("MQTT: set the temperature");
+
+    for(int i=0; i<length; i++){
+      rxVal[i] = payload[i];
+    }
+
+    targetTemperature = atof(rxVal);
+    Serial.print("Set targetTemperature: ");
+    Serial.println(targetTemperature);
+  }
+
+}
+void manageDisconnections(){
+
+}
+void manageQueueSubscription(){
+    bootstrapManager.subscribe(tubCommandTopic.c_str());
+    Serial.print("Subscribed to ");
+    Serial.println(tubCommandTopic);
+}
+void manageHardwareButton(){
+
 }
--- src/HotTubControl.h
+++ src/HotTubControl.h
@@ -1,1 +1,57 @@
 #include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include "BootstrapManager.h"
+
+// sensor inputs; D0 can only be used as a read/write. can't be used as interrupt or one wire
+#define STATUS_HEAT1 D0 // pulled low by optocoupler when heat 1 relay energized
+#define STATUS_PUMPLOW D1 // pulled low by optocoupler when pump low relay energized
+#define STATUS_PUMPHIGH D0 // pulled low by optocoupler when pump high relay energized
+#define TEMPERATURE D3 // Dallas one wire temp sensor
+#define PRESSURE D4 // input dry contact for pressure switch used to detect water flow (pump is running)
+// #define STATUS_HEAT2 // not enough GPIOs pulled low by optocoupler when heat 2 relay energized
+
+// relay outputs
+#define HEAT1 D5 // output A to trigger the Heater 1 relay
+#define HEAT2 D6 // output B to trigger the Heater 2 relay
+#define PUMPLOW D7 // output C to trigger the Pump Low Speed Relay
+#define PUMPHIGH D8 // output D to trigger the Pump High Speed Relay
+
+bool setupComplete = false;
+String tubCommandTopic = "hottub/command/#";
+String tubStatusTopic = "hottub/status";
+
+float actualTemperature = 101.1; // set init value to 101, to be overwritten when actual temp is taken
+float targetTemperature = 101.1; // default to 101 degrees as desired set temperature
+const float tempCutIn = 1.1; // number of degrees to start / stop heating from
+const int statusTempInterval = 5000; // number of millis to wait before sending temp update on mqtt
+
+unsigned long int lastHeatMillis = 0;
+bool statusPressure = 0; // is pressure detected or not. if true, pump is running. if false, pump is not running
+bool statusPumpLow = 0;
+bool statusPumpHigh = 0;
+bool statusHeat1 = 0;
+
+void statusUpdateLoop(); // run in main loop to send mqtt messages when the value of STATUS PUMP/HEAT changes
+
+/****************** BOOTSTRAP MANAGER ******************/
+BootstrapManager bootstrapManager;
+
+/********************************** BOOTSTRAP FUNCTION DECLARATION *****************************************/
+void callback(char *topic, byte *payload, unsigned int length);
+void manageDisconnections();
+void manageQueueSubscription();
+void manageHardwareButton();
+
+
+/********************************** INTERRUPT SERVICE ROUTINES ***********************************/
+void IRAM_ATTR isrPressureDetected();
+void IRAM_ATTR isrPumpLow();
+void IRAM_ATTR isrPumpHigh();
+void IRAM_ATTR isrHeat1();
+
+
+// DS18B20 Temperature Sensor
+OneWire oneWire(TEMPERATURE);
+DallasTemperature sensors(&oneWire);
+void updateTemperatureLoop();
